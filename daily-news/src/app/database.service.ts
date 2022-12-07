76f8de2d32f6f35b20e6e57c0a3cabf61abc4038
Your Name <agandhi29@myseneca.ca>
2022-12-07 04:42:39 -0500

    need to fix

--- daily-news/src/app/database.service.ts
+++ daily-news/src/app/database.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { User } from './User';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DatabaseService {
+  private url = 'https://daily-news-database-service.onrender.com';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getUser(id: any): Observable<User> {
+    return this.http.get<User>(this.url + '/api/users/' + id).pipe(
+      catchError((err) => {
+        console.log('Handling error locally and rethrowing it...', err);
+        return throwError(err);
+      })
+    );
+  }
+
+  newUser(data: User): Observable<any> {
+    return this.http.post<any>(this.url + '/api/users', data);
+  }
 }
--- daily-news/src/app/user-profile/user-profile.component.ts
+++ daily-news/src/app/user-profile/user-profile.component.ts
@@ -1,36 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
+import { DatabaseService } from '../database.service';
 import { User } from '../User';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.css']
+  styleUrls: ['./user-profile.component.css'],
 })
 export class UserProfileComponent implements OnInit {
-  constructor(public auth : AuthService) {}
+  constructor(
+    public auth: AuthService,
+    private databaseService: DatabaseService,
+    private router: Router
+  ) {}
   user_: User = {
-    id: "",
-    name: "",
-    email: "",
-    img: "",
+    id: '',
+    name: '',
+    email: '',
+    img: '',
     prefs: {
-        locs: ["Milwaukee,US","London,GB","Paris,FR"],
-        categs: ["Technology","Culture","Business","Style"]
-    }
+      locs: ['Milwaukee,US', 'London,GB', 'Paris,FR'],
+      categs: ['Technology', 'Culture', 'Business', 'Style'],
+    },
   };
 
   ngOnInit(): void {
-    this.auth.user$.subscribe(
-      (profile)=> 
-        {
-          this.user_.id = profile?.sub;
-          this.user_.name = profile?.name;
-          this.user_.img = profile?.picture;
-          this.user_.email = profile?.email;
-        }
-    ) 
+    this.auth.user$.subscribe((profile) => {
+      if (profile?.sub != null) {
+        this.user_.id = profile?.sub;
+        this.user_.name = profile?.name;
+        this.user_.img = profile?.picture;
+        this.user_.email = profile?.email;
+        this.databaseService.getUser(this.user_.id).subscribe((data) => {
+          if (data == null) {
+            this.databaseService.newUser(this.user_).subscribe((err) => {
+              console.log(err);
+            });
+          } else {
+            this.user_.prefs = data.prefs;
+          }
+        });
+      } else {
+        this.router.navigate(['/']);
+      }
+    });
   }
 }
-
-
