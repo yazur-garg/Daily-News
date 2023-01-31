3f818df81670eb7de3cdefc6d41a2b06306772dc
AParris21 <>
2023-01-31 11:48:34 -0500

    Updated Routing mod with Search component

--- daily-news/src/app/app-routing.module.ts
+++ daily-news/src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SearchComponent } from './search/search.component';
 
 const routes: Routes = [
   { path:'home' , component: HomeComponent },
   { path:'login' , component: LoginComponent },
   { path:'profile', component: UserProfileComponent },
   { path:'home/:category' , component: HomeComponent },
-  { path:'search/:topic' , component: HomeComponent },
+  { path:'search/:topic' , component: SearchComponent },
   { path:'' , redirectTo: '/home', pathMatch: 'full' },
   { path:'**' , component: PageNotFoundComponent }
 ];
