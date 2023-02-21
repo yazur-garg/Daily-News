873a6cca6c9ed121501fff568d8213efbfd49867
AParris21 <>
2023-02-21 07:11:50 -0500

    Added 'getWeather()' function to weather service

--- daily-news/src/app/weather.service.ts
+++ daily-news/src/app/weather.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getWeather(lat : Number, lon : Number, limit : Number = 1): Observable<any> {
+    return this.http.get<any>(`http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=${limit}&appid={API key}`)
+                  .pipe(catchError(err => {
+                     console.log('Handling error locally and rethrowing it...',err);
+                     return throwError(err)}));
+  }
 }
