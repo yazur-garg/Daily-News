7dfa1427c2443502e105c6d8c61873da2349e8f3
Yazur Garg <gyazur@gmail.com>
2023-02-21 02:28:24 -0500

    adding part to get the current location

--- daily-news/src/app/header/header.component.ts
+++ daily-news/src/app/header/header.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
+import { Geolocation } from '@capacitor/geolocation';
+import { HttpClient } from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -9,16 +12,30 @@ import { AuthService } from '@auth0/auth0-angular';
 export class HeaderComponent implements OnInit {
 
   profileJson: string = "";
+  position: any = {
+    lat: 43.7001,
+    lng: -79.4163
+  }
 
-  constructor(public auth: AuthService) { }
+  constructor(public auth: AuthService, private http: HttpClient) { }
 
   ngOnInit(): void {
+    Geolocation.getCurrentPosition().then((position) => {
+      this.position.lat = position.coords.latitude;
+      this.position.lng = position.coords.longitude;
+      console.log('Latitude: ' + position.coords.latitude);
+      console.log('Longitude: ' + position.coords.longitude);
+      console.log(this.position);
+      
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
 
     this.auth.user$.subscribe(
       (profile) => (this.profileJson = JSON.stringify(profile, null, 2)),
     );
   }
-
+  
   formSubmit() {
     console.log('form submitted');
   }
